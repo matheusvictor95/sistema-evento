7a424fd430d72b81dc154d170d8264007e79d600
matheus.diniz <matheusv.diniz95@gmail.com>
2025-07-22 16:58:30 -0300

    feat: metodo edit e update de eventos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Http\Requests\StoreEventoRequest;
+use App\Http\Requests\UpdateEventoRequest;
 use App\Models\Evento;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
@@ -27,16 +28,13 @@ class EventoController extends Controller
     
     public function create()
     {
-    
         return Inertia::render('Evento/Create');
     }
 
     public function store(StoreEventoRequest $request)
     {
-        $evento = Evento::create($request->validated());
-        return Inertia::render('Evento/Index',[
-            'evento' => $evento,
-        ]);
+        Evento::create($request->validated());
+        return to_route('evento.index');
        
     }
 
@@ -46,5 +44,20 @@ class EventoController extends Controller
      return Inertia::render('Evento/Show', [
         'evento' => $id
      ]);
+    }
+
+    public function edit($id)
+    {
+        $evento = Evento::find($id);
+        return Inertia::render('Evento/Edit', [
+            'evento' => $evento
+        ]);
+    }
+
+    public function update(UpdateEventoRequest $request, $id)
+    {
+        $evento = Evento::find($id);
+        $evento->update($request->validated());
+        return to_route('evento.index');
     }
 }
